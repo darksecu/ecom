4d1023fff1cbaacd153b414c9a673b3a1d5e15a3
Rahul Shandilya <rahul.shandilya.p@pimcore.services>
2022-12-13 15:17:31 +0530

    linting

--- src/presentationals/screens/Cart/index.tsx
+++ src/presentationals/screens/Cart/index.tsx
@@ -1,18 +1,8 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
-import {useAppSelector, useAppDispatch} from '../../../store/hooks';
-import {fetchPLP} from '../../../store/slices/plp';
 import {CartTemplate} from '../../templates';
 
 const CartScreen = () => {
-  const dispatch = useAppDispatch();
-  const list = useAppSelector(state => state.plpReducer.list);
-  const isLoading = useAppSelector(state => state.plpReducer.isLoading);
-
-  useEffect(() => {
-    dispatch(fetchPLP({}));
-  }, []);
-
   return (
     <SafeAreaView style={{flex: 1}}>
       <StatusBar />
