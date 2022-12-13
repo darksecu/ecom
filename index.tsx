198095be251fa48d3d2d5566eacb73d3b30bf483
Rahul Shandilya <rahul.shandilya.p@pimcore.services>
2022-12-13 14:43:21 +0530

    persist

--- index.tsx
+++ index.tsx
@@ -1,13 +1,16 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import {store} from './src/store';
+import {store, persistor} from './src/store';
 import {Provider} from 'react-redux';
 import RootNavigation from './src/navigation';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const AppWrapper = () => {
   return (
     <Provider store={store}>
-      <RootNavigation />
+      <PersistGate loading={null} persistor={persistor}>
+        <RootNavigation />
+      </PersistGate>
     </Provider>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,27 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import plpReducer from './slices/plp';
 import cartReducer from './slices/cart';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas/plp';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const rootReducer = combineReducers({
+  plpReducer: plpReducer,
+  cartReducer: cartReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    plpReducer: plpReducer,
-    cartReducer: cartReducer,
-  },
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware => [
     ...getDefaultMiddleware({
       serializableCheck: false,
@@ -22,6 +33,7 @@ export const store = configureStore({
 
 sagaMiddleware.run(rootSaga);
 
+export const persistor = persistStore(store);
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
